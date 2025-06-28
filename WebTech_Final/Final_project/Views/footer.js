2d5fa71e3574e758dfb06304313bcf58f43065e0
KaziTanzil <kazitanjilkazitanjil@gmail.com>
2025-06-29 02:57:18 +0600

    Remove deleted files

--- WebTech_Final/Final_project/Views/footer.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-    document.body.classList.toggle('dark-theme');
-    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
-}
-
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-theme');
-}
